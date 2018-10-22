bb863053a3104961f7d9af927c5459b0c04f6104
abstract-animation-9914 <vm930@hotmail.com>
2018-10-22 22:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentloaded", (e) => {
   // invoking a function here will make sure it happens on page load
   // your code here
+  const title = document.querySelector("#title");
+  title.innerHTML = `<title>${movie.titanic}</title>`
 });
